8d5bd976526c4217188c22c6f3d149a4a5541159
anthony29p <anthony.perez@uni.pe>
2022-10-02 22:38:40 -0500

    validations

--- src/functions/postCard.ts
+++ src/functions/postCard.ts
@@ -1,28 +1,52 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
 import { connectDataBaseModel } from "src/database";
-import { creditCard } from "../database/schema/CreditCard";
+import { dataValidator } from 'src/validations/cardValidation';
+import creditCardSchema from '../database/schema/CreditCard';
+
 
 const { v4 } = require('uuid');
+const jwt =  require('jsonwebtoken')
 
-export const postCard = async (event,_context) => {
+export const postCard: APIGatewayProxyHandler = async (event,_context) => {
   
   let { card_number,cvv,expiration_month,expiration_year,email } = JSON.parse(event.body);
-  const id = v4()
+  let error =dataValidator(card_number,cvv,expiration_month,expiration_year,email)
 
-  const newCard = {
-    id,
-    card_number,
-    cvv,
-    expiration_month,
-    expiration_year,
-    email,
+  
+  if(error==='no errors'){
+    const id = v4()
+    const token:string =jwt.sign({id},'tokenKey',{expiresIn:15*60*1000})
+
+    const createdAt = new Date();
+    
+    const Modelo = await connectDataBaseModel('CreditCardTest',creditCardSchema)
+
+    const newCard = {
+      id,
+      card_number,
+      cvv,
+      expiration_month,
+      expiration_year,
+      email,
+      createdAt,
+    }
+    const newCreditCard = new Modelo(newCard)
+    await newCreditCard.save();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newCreditCard),
+      headers: {
+        'auth-token': token,
+      },
+    };
   }
-
-  const Modelo = await connectDataBaseModel('CreditCardTest',creditCard)
-  const newCreditCard = new Modelo(newCard)
-  await newCreditCard.save();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(newCreditCard),
-  };
+  else{
+    return{
+      statusCode: 400,
+      body: JSON.stringify(error),
+    }
+  }
+  
+  
 };
